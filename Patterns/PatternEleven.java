1afca77912c69febdfbf14f4d5884ef7ce3b7539
SanthiyaMeganathan <120907788+SanthiyaMeganathan@users.noreply.github.com>
2025-01-21 12:19:47 +0530

    pattern eleven

---
+++ Patterns/PatternEleven.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class PatternEleven{
+    public static void main(String[] args) {
+        System.out.println("Enter the value n: ");
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        pattern(n);
+
+    }
+    
+    static void pattern(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+
+            }
+            System.out.println();
+
+        }
+        
+        for (int i = 1; i <= n - 1; i++) {
+            for (int j = n - i; j >= 1; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+    }
+    
+
+}
